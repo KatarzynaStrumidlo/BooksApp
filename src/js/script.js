7b27245819cfe3d34fcd21b72377a4d55121d2f3
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-02-11 11:21:17 +0100

    Add favorite books and initAction function

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+{
 'use strict';
 const select = {
   templateOf: {
@@ -13,17 +14,44 @@ const templates = {
 
 function render(){
   for(let books of dataSource.books){
-     /* generate HTML based on template */
-     const generatedHTML = templates.booksTemplate(books);
-     /* create element using utils.createElementFromHTML */
-     const element = utils.createDOMFromHTML(generatedHTML);
-     /* find menu comtainer */
-     const booksContainer = document.querySelector(select.containerOf.booksList);
-     /* add element to menu */
-     booksContainer.appendChild(element);
+    /* generate HTML based on template */
+    const generatedHTML = templates.booksTemplate(books);
+    /* create element using utils.createElementFromHTML */
+    const element = utils.createDOMFromHTML(generatedHTML);
+    /* find menu comtainer */
+    const booksContainer = document.querySelector(select.containerOf.booksList);
+    /* add element to menu */
+    booksContainer.appendChild(element);
   }
 }
 render();
+
+const favoriteBooks = [];
+
+function initActions(){
+  const booksContainer = document.querySelector(select.containerOf.booksList);
+  const booksImage = booksContainer.querySelectorAll('.book__image');
+  for (let image of booksImage){
+    image.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      image.classList.add('favorite');
+      const id = image.getAttribute('data-id');
+      favoriteBooks.push(id);
+      console.log('działa nasłuchiwacz');
+    });
+  }
+/* niby wszystko działa, ale jak klikam na tą ksiażkę w drugim rzędzie
+to coś się psuje. Jak jest na szerokim ekranie i klikam książkę nr 4
+(jest wtedy w górnym rzędzie) to działa, ale jak zmiejszę ekran i przeskakuje
+ona do drugiego rzędu to przestaje działać */
+}
+initActions();
+console.log(favoriteBooks);
+
+
+
+
+}
 
 
 
