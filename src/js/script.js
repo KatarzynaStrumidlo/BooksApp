42a4e4ee906e6de34dc581c7ac7728c613d5ed12
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-02-11 18:05:36 +0100

    Add filter to books in function filterBooks

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
     },
     containerOf: {
       booksList : '.books-list',
+      filters: '.filters',
     },
   };
   const templates = {
@@ -27,6 +28,7 @@
   render();
 
   const favoriteBooks = [];
+  const filters = [];
 
   function initActions(){
     const booksContainer = document.querySelector(select.containerOf.booksList);
@@ -42,14 +44,49 @@
         clickedElem.classList.remove('favorite');
       }
     });
-
-  /* niby wszystko działa, ale jak klikam na tą ksiażkę w drugim rzędzie
+    /* niby wszystko działa, ale jak klikam na tą ksiażkę w drugim rzędzie
   to coś się psuje. Jak jest na szerokim ekranie i klikam książkę nr 4
   (jest wtedy w górnym rzędzie) to działa, ale jak zmiejszę ekran i przeskakuje
   ona do drugiego rzędu to przestaje działać */
+    const filtersContainer = document.querySelector(select.containerOf.filters);
+    filtersContainer.addEventListener('click', function(event){
+      const clickedElem = event.target;
+      if(clickedElem.tagName === 'INPUT' && clickedElem.type === 'checkbox' && clickedElem.name ==='filter'){
+        console.log(clickedElem.value);
+        if(clickedElem.checked){
+          filters.push(clickedElem.value);
+          console.log('filters', filters);
+        } else {
+          const id = filters.indexOf(clickedElem.value);
+          filters.splice(filters.indexOf(id), 1);
+          console.log('filters', filters);
+        }
+      }
+      filterBooks();
+    });
   }
   initActions();
   console.log(favoriteBooks);
+
+  function filterBooks(){
+    for(let book of dataSource.books){
+      let shouldBeHidden = false;
+      for(let filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+      if(shouldBeHidden){
+        document.querySelector('.book__image[data-id="' + book.id + '"]').classList.add('hidden');
+      }else {
+        document.querySelector('.book__image[data-id="' + book.id + '"]').classList.remove('hidden');
+      }
+    }
+  }
+
+
+
 
 
 
