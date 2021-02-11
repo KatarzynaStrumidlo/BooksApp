788841123abdf93a286983eee45346ade1d641bd
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-02-11 18:49:36 +0100

    Convert to OOP and add function determineRatingBgc doesn't work

--- src/js/script.js
+++ src/js/script.js
@@ -13,77 +13,109 @@
     booksTemplate : Handlebars.compile(document.querySelector(select.templateOf.booksTemplate).innerHTML),
   };
 
-  function render(){
-    for(let books of dataSource.books){
-    /* generate HTML based on template */
-      const generatedHTML = templates.booksTemplate(books);
-      /* create element using utils.createElementFromHTML */
-      const element = utils.createDOMFromHTML(generatedHTML);
-      /* find menu comtainer */
-      const booksContainer = document.querySelector(select.containerOf.booksList);
-      /* add element to menu */
-      booksContainer.appendChild(element);
+  class BooksList {
+    constructor() {
+      const thisBooksList = this;
+      thisBooksList.initData();
+      thisBooksList.getElements();
+      thisBooksList.render();
+      thisBooksList.initActions();
+      thisBooksList.determineRatingBgc();
     }
-  }
-  render();
 
-  const favoriteBooks = [];
-  const filters = [];
+    initData() {
+      const thisBooksList = this;
+      thisBooksList.data = dataSource.books;
+    }
 
-  function initActions(){
-    const booksContainer = document.querySelector(select.containerOf.booksList);
-    booksContainer.addEventListener('dblclick', function(event){
+    getElements() {
+      const thisBooksList = this;
+      thisBooksList.booksContainer = document.querySelector(select.containerOf.booksList);
+      thisBooksList.filtersContainer = document.querySelector(select.containerOf.filters);
+      thisBooksList.favoriteBooks = [];
+      thisBooksList.filters = [];
+      thisBooksList.filterBook = document.querySelector('.book__image[data-id="' + book.id + '"]');
+    }
+
+    render(){
+      const thisBooksList = this;
+      for(let books of dataSource.books){
+      /* generate HTML based on template */
+        const generatedHTML = templates.booksTemplate(books);
+        /* create element using utils.createElementFromHTML */
+        const element = utils.createDOMFromHTML(generatedHTML);
+        /* add element to container */
+        thisBooksList.booksContainer.appendChild(element);
+
+        const ratingBgc = determineRatingBgc(rating);
+        const ratingWidth = width;
+      };
+    }
+
+    initActions() {
+      const thisBooksList = this;
+      thisBooksList.booksContainer.addEventListener('dblclick', function(event){
       event.preventDefault();
       const clickedElem = event.target.offsetParent;
       const id = clickedElem.getAttribute('data-id');
       if(!clickedElem.classList.contains('favorite')){
-        favoriteBooks.push(id);
+        thisBooksList.favoriteBooks.push(id);
         clickedElem.classList.add('favorite');
       } else {
-        favoriteBooks.splice(favoriteBooks.indexOf(id), 1);
+        thisBooksList.favoriteBooks.splice(thisBooksList.favoriteBooks.indexOf(id), 1);
         clickedElem.classList.remove('favorite');
       }
     });
     /* niby wszystko działa, ale jak klikam na tą ksiażkę w drugim rzędzie
-  to coś się psuje. Jak jest na szerokim ekranie i klikam książkę nr 4
-  (jest wtedy w górnym rzędzie) to działa, ale jak zmiejszę ekran i przeskakuje
-  ona do drugiego rzędu to przestaje działać */
-    const filtersContainer = document.querySelector(select.containerOf.filters);
-    filtersContainer.addEventListener('click', function(event){
+    to coś się psuje. Jak jest na szerokim ekranie i klikam książkę nr 4
+    (jest wtedy w górnym rzędzie) to działa, ale jak zmiejszę ekran i przeskakuje
+    ona do drugiego rzędu to przestaje działać */
+
+      thisBooksList.filtersContainer.addEventListener('click', function(event){
       const clickedElem = event.target;
       if(clickedElem.tagName === 'INPUT' && clickedElem.type === 'checkbox' && clickedElem.name ==='filter'){
         console.log(clickedElem.value);
         if(clickedElem.checked){
-          filters.push(clickedElem.value);
-          console.log('filters', filters);
+          thisBooksList.filters.push(clickedElem.value);
         } else {
-          const id = filters.indexOf(clickedElem.value);
-          filters.splice(filters.indexOf(id), 1);
-          console.log('filters', filters);
+          const id = thisBooksLi.stfilters.indexOf(clickedElem.value);
+          thisBooksList.filters.splice(thisBooksList.filters.indexOf(id), 1);
         }
       }
-      filterBooks();
     });
-  }
-  initActions();
-  console.log(favoriteBooks);
+    }
 
-  function filterBooks(){
-    for(let book of dataSource.books){
-      let shouldBeHidden = false;
-      for(let filter of filters){
-        if(!book.details[filter]){
-          shouldBeHidden = true;
-          break;
+    filterBooks() {
+      const thisBooksList = this;
+      for(let book of dataSource.books){
+        let shouldBeHidden = false;
+        for(let filter of filters){
+          if(!book.details[filter]){
+            shouldBeHidden = true;
+            break;
+          }
         }
-      }
-      if(shouldBeHidden){
-        document.querySelector('.book__image[data-id="' + book.id + '"]').classList.add('hidden');
-      }else {
-        document.querySelector('.book__image[data-id="' + book.id + '"]').classList.remove('hidden');
+        if(shouldBeHidden){
+          thisBooksList.filterBook.classList.add('hidden');
+        }else {
+          thisBooksList.filterBook.classList.remove('hidden');
+        }
       }
     }
+
+    determineRatingBgc(rating) {
+      ...
+    }
+
   }
+
+  const app = new BooksList();
+
+
+
+
+
+
 
 
 
